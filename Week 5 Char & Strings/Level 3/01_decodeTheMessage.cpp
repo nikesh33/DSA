545d4b0b7bc45eacfd31724ba7b9ed7cccb92d22
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-07-16 23:38:41 +0530

    Q Minimum Amount of Time to Collect Garbage

---
+++ Week 5 Char & Strings/Level 3/01_decodeTheMessage.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    string decodeMessage(string key, string message)
+    {
+
+        // create mapping
+        char start = 'a';
+        char mapping[280] = {0};
+
+        for (auto ch : key)
+        {
+            if (ch != ' ' && mapping[ch] == 0)
+            {
+                mapping[ch] = start;
+                start++;
+            }
+        }
+
+        // use mapping
+        string ans;
+        for (auto ch : message)
+        {
+            if (ch == ' ')
+            {
+                ans.push_back(' ');
+            }
+            else
+            {
+                char decodedChar = mapping[ch];
+                ans.push_back(decodedChar);
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string key = "the quick brown fox jumps over the lazy dog";
+    string message = "vkbs bs t suepuv";
+    cout << sol.decodeMessage(key, message) << endl;
+
+    return 0;
+}
