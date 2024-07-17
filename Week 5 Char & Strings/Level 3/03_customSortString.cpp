cb5c5dad89447bc1f5cb0abd06453b1cd1449bd0
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-07-17 23:55:12 +0530

    Q3

--- Week 5 Char & Strings/Level 3/03_customSortString.cpp
+++ Week 5 Char & Strings/Level 3/03_customSortString.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm> // Required for std::sort
 using namespace std;
 
-int main()
-{
+class Solution {
+public:
+    // custom comparator
+    static string str;
+    static bool compare(char char1, char char2) {
+        // this will return true, if position of character1 in str string is
+        // less than the position of character2 in str string
+
+        // when true is returned, then char1 will be placed before char2 in
+        // output string
+        return (str.find(char1) < str.find(char2));
+    }
+    string customSortString(string order, string s) {
+
+        str = order;
+        sort(s.begin(), s.end(), compare);
+        return s;
+    }
+};
+
+string Solution::str;
+
+int main() {
+    Solution solution;
+    string order = "cba";
+    string s = "abcd";
+    string sortedString = solution.customSortString(order, s);
+    cout << "Sorted string: " << sortedString << endl; // Expected output: "cbad"
+    return 0;
 }
