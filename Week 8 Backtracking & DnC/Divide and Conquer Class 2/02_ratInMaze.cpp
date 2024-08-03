8e398267cb909209bb8d5c3c6348c650b24c2cc0
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-08-03 23:22:41 +0530

    Timestamp 01:23

--- Week 8 Backtracking & DnC/Divide and Conquer Class 2/02_ratInMaze.cpp
+++ Week 8 Backtracking & DnC/Divide and Conquer Class 2/02_ratInMaze.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void printAllPath(int maze[][4], int row, int col, int srcx, int srcy, string &output)
+// a function that will handle all the below mentioned possibilities:
+// Possiblity hai
+// -> path closed
+// -> out of bound
+
+bool isSafe(int srcx, int srxy, int newx, int newy, int maze[][4], int row, int col, vector<vector<bool>> &visited)
 {
+    if (
+        (newx >= 0 && newx < row) &&
+        (newy >= 0 && newy < col) &&
+        maze[newx][newy] == 1 &&
+        visited[newx][newy] == false)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void printAllPath(int maze[][4], int row, int col, int srcx, int srcy, string &output, vector<vector<bool>> &visited)
+{
+
     // base case
     // destination cordinates are[row-1],[col-1]
     if (srcx == row - 1 && srcy == col - 1)
@@ -15,13 +38,41 @@ void printAllPath(int maze[][4], int row, int col, int srcx, int srcy, string &o
     //   1 case solve karo baki recursion solve karega
 
     // UP
-    
-
+    int newx = srcx - 1;
+    int newy = srcy;
+    if (isSafe(srcx, srcy, newx, newy, maze, row, col, visited))
+    {
+        // mark visited
+        visited[newx][newy] = true;
+        // call recursion
+        printAllPath(maze,row,col,newx,newy,output + 'U',visited);
+        // backtracking
+          visited[newx][newy] = false ;
+    }
+  
     // RIGHT
+    if (isSafe())
+    {
+        // mark visited
+        // call recursion
+        // backtracking
+    }
 
     // DOWN
+    if (isSafe())
+    {
+        // mark visited
+        // call recursion
+        // backtracking
+    }
 
     // LEFT
+    if (isSafe())
+    {
+        // mark visited
+        // call recursion
+        // backtracking
+    }
 }
 
 int main()
@@ -38,7 +89,19 @@ int main()
 
     int srcx = 0;
     int srcy = 0;
-
     string output = "";
-    printAllPath(maze, row, col, srcx, srcy, output);
+
+    // create visited 2D Array
+    vector<vector<bool>> visited(row, vector<bool>(col, false));
+
+    if (maze[0][0] == 0)
+    {
+        // src ki position is closed, taht means Rat cannot move
+        cout << "No Path Exists" << endl;
+    }
+    else
+    {
+        visited[srcx][srcy] = true;
+        printAllPath(maze, row, col, srcx, srcy, output);
+    }
 }
