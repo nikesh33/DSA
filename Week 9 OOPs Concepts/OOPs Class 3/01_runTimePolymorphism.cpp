df0b73cdc8cf8549923172a48b4938aab9536c1b
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-08-14 22:21:04 +0530

    Run Time Polymorphism

--- Week 9 OOPs Concepts/OOPs Class 3/01_runTimePolymorphism.cpp
+++ Week 9 OOPs Concepts/OOPs Class 3/01_runTimePolymorphism.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Animal
 {
 public:
-    void sound()
+    virtual void sound()
     {
         cout << "Animal Making Sound" << endl;
     }
@@ -13,7 +13,7 @@ public:
 class Dog : public Animal
 {
 public:
-    void sound()
+    void sound() override
     {
         cout << "Dog Barking" << endl;
     }
@@ -22,19 +22,27 @@ public:
 class Cat : public Animal
 {
 public:
-    void sound()
+    void sound() override
     {
         cout << "Cat Meowing" << endl;
     }
 };
 
-void sound(){
-    
+void sound(Animal *animal)
+{
+    animal->sound(); //polymorphic
+    // animal-> sound is behaving as per required object allocated at runtime
 }
 
 int main()
 {
-    Dog *dog = new Dog();
-    dog->sound();
+    // Dog *dog = new Dog();
+    // dog->sound();
+
+    Animal *animal = new Dog();
+    sound(animal);
+    animal = new Cat();
+    sound(animal);
+
     return 0;
 }
