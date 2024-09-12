96d7ff578e2a1418c6bd448ed46e57807b19a163
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-09-12 23:21:09 +0530

    added preOrder and InOrder Traversal Ans Kth ancestor

---
+++ Week 13 Generic & Binary Trees/Generic & Binary Tree Class 2/06_preOrderAndInorderTraversal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    int inorder[] = {10,8,6,2,4,12};
+    int preOrder[] = {2,8,10,6,4,12};
+    int size = 6;
+    int preOrderIndex = 0;
+    int inorderStart = 0;
+    int inorderEnd = 5;
+       
+
+    return 0;
+}
