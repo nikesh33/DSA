92a03ea0c137d55f8958efa5e8bd6a77ffae0575
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-10-10 23:53:32 +0530

    Edit Distance Solved Using Tabulation

--- Week 17 DP Level 1/Dynamic Programming Class 4/03_editDistance.cpp
+++ Week 17 DP Level 1/Dynamic Programming Class 4/03_editDistance.cpp
@@ -69,6 +69,83 @@ public:
         return dp[i][j];
     }
 
+    // Using Tabulation
+    int solveUsingTabulation(string a, string b)
+    {
+        vector<vector<int>> dp(a.length() + 1, vector<int>(b.length() + 1, -1));
+
+        for (int col = 0; col <= b.length(); col++)
+        {
+            dp[a.length()][col] = b.length() - col;
+        }
+
+        for (int row = 0; row <= a.length(); row++)
+        {
+            dp[row][b.length()] = a.length() - row;
+        }
+
+        for (int i_index = a.length() - 1; i_index >= 0; i_index--)
+        {
+            for (int j_index = b.length() - 1; j_index >= 0; j_index--)
+            {
+                int ans = 0;
+                if (a[i_index] == b[j_index])
+                {
+                    ans = 0 + dp[i_index + 1][j_index + 1];
+                }
+                else
+                {
+                    int replace = 1 + dp[i_index + 1][j_index + 1];
+                    int insert = 1 + dp[i_index][j_index + 1];
+                    int remove = 1 + dp[i_index + 1][j_index];
+                    ans = min(insert, min(remove, replace));
+                }
+                dp[i_index][j_index] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+
+    // Space Optimisation
+    int solveUsingTabulationSO(string a, string b)
+    {
+        // vector<vector<int>> dp(a.length() + 1, vector<int>(b.length() + 1, -1));
+        vector<int> curr(a.length() + 1, 0);
+        vector<int> next(a.length() + 1, 0);
+
+        // // abhi ke liye bhul jao
+        // for (int col = 0; col <= b.length(); col++)
+        // {
+        //     dp[a.length()][col] = b.length() - col;
+        // }
+
+        for (int row = 0; row <= a.length(); row++)
+        {
+            next[row] = a.length() - row;
+        }
+
+        for (int i_index = a.length() - 1; i_index >= 0; i_index--)
+        {
+            for (int j_index = b.length() - 1; j_index >= 0; j_index--)
+            {
+                int ans = 0;
+                if (a[i_index] == b[j_index])
+                {
+                    ans = 0 + dp[i_index + 1][j_index + 1];
+                }
+                else
+                {
+                    int replace = 1 + dp[i_index + 1][j_index + 1];
+                    int insert = 1 + dp[i_index][j_index + 1];
+                    int remove = 1 + dp[i_index + 1][j_index];
+                    ans = min(insert, min(remove, replace));
+                }
+                dp[i_index][j_index] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     int minDistance(string word1, string word2)
     {
         int i = 0;
@@ -77,8 +154,14 @@ public:
         // int ans = solveUsingRecursion(word1, word2, i, j);
 
         // // Using Memoisation
-        vector<vector<int>> dp(word1.length() + 1, vector<int>(word2.length() + 1, -1));
-        int ans = solveUsingMem(word1, word2, i, j, dp);
+        // vector<vector<int>> dp(word1.length() + 1, vector<int>(word2.length() + 1, -1));
+        // int ans = solveUsingMem(word1, word2, i, j, dp);
+
+        // // Using Tabulation
+        // int ans = solveUsingTabulation(word1, word2);
+
+        // Space Optimisation
+        int ans = solveUsingTabulationSO(word1, word2);
         return ans;
     }
 };
